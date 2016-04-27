ebebac60fbeaa1a4f2915b53cf7bfc18d369a21e
John Boucherie <jvboucherie@gmail.com>
2016-04-27 15:56:01 -0700

    Add example controller for testing purposes.

---
+++ client/example/example_controller.js
@@ -0,0 +1,29 @@
+(function(){
+  angular.module('my-module', []);
+
+  angular
+    .module('my-module')
+    .controller('MyController', [
+      '$scope',
+      function($scope){
+        let self = this;
+
+        self.firstName = '';
+        self.lastName = '';
+
+        self.getFullName = function(){
+          return self.firstName + ' ' + self.lastName;
+        };
+
+        $scope.songs = [
+        'Here Comes The Sun'
+        ];
+
+        $scope.addSong = function(song) {
+          $scope.songs.push(song);
+        };
+
+        return self;
+      }
+  ]);
+})();
