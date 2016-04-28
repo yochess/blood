17c4d61ac96c9c06e6f4fe4f9d59f9f76e880708
vinithasraja <snellhome1@gmail.com>
2016-04-27 18:22:37 -0700

    Add angular routes

--- client/app.js
+++ client/app.js
@@ -1,1 +1,24 @@
-let app = angular.module('blood', []);
+const app = angular.module('blood', [
+  'ngRoute'
+  ])
+
+.config(function ($routeProvider, $httpProvider) {
+  $routeProvider
+
+  .when('/', {
+    templateUrl: 'bloodMap/bloodMap.html',
+    controller: 'BloodMapController',
+  })
+
+  .when('/profile', {
+      templateUrl: 'profile/profile.html',
+      controller: 'ProfileController',
+      controllerAs: 'ProfileCtrl'
+  })
+
+    .otherwise({
+      redirectTo: '/'
+  });
+
+
+});
