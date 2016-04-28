e30b774db378324263337d62baa2df9ed0393330
John Boucherie <jvboucherie@gmail.com>
2016-04-28 12:28:31 -0700

    Add landing page scaffolding.

---
+++ client/Landing/landing_controller.js
@@ -0,0 +1,8 @@
+app.controller('LandingController', ['$routeParams', function($routeParams) {
+
+  let LandingCtrl = this;
+
+  LandingCtrl.github = 'https://github.com/britishchicken/blood';
+
+}]);
+
