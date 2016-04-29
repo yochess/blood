55b2bb2fc9df420dbc26cc2c966d7e514fa25899
vinithasraja <snellhome1@gmail.com>
2016-04-28 17:06:36 -0700

    Implement initializeMaps to display google Maps

--- client/bloodMap/bloodMap_controller.js
+++ client/bloodMap/bloodMap_controller.js
@@ -1,5 +1,23 @@
-app.controller('BloodMapController', ['$routeParams', function($routeParams) {
-    console.log('testing bloodMap');  
+
+app.controller('BloodMapController', ['$routeParams', function($routeParams, $rootScope) {
+
+  let BloodMapCtrl = this;
+
+  let initializeMaps = function () {
+    let mapProp = {
+            center:new google.maps.LatLng(51.508742,-0.120850),
+            zoom:5,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+          };
+    BloodMapCtrl.map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    google.maps.event.addDomListener(window, 'load', initializeMaps);
+    
+  };
+
+  initializeMaps();
 }]);
+
+
+
 
 
