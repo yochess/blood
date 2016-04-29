7902152c5e42ac581e780ed4bfc1969f42e7c3b8
Daham Drake Wang <drakewang@gmail.com>
2016-04-28 17:25:40 -0700

    Implement tests to check if user is logged in or logged out.

---
+++ client/navbar/navbar.tests.js
@@ -0,0 +1,35 @@
+describe('NavController', () => {
+  beforeEach(module('blood'));
+
+  describe('NavCtrl.logInOrOut()', () => {
+    it('should log the user in when the user is logged out', inject(($controller) => {
+      localStorage.setItem('id', '');
+      localStorage.setItem('type', '');
+
+      let navController = $controller('NavController', {
+        $window: {
+          localStorage: localStorage
+        }
+      });
+
+      navController.logInOrOut();
+      localStorage.getItem('id').should.equal('something');
+      localStorage.clear();
+    }));
+
+    it('should log the user out when the user is logged in', inject(($controller) => {
+      localStorage.setItem('id', 'something');
+      localStorage.setItem('type', 'something');
+
+      let navController = $controller('NavController', {
+        $window: {
+          localStorage: localStorage
+        }
+      });
+
+      navController.logInOrOut();
+      localStorage.getItem('id').should.equal('');
+      localStorage.clear();      
+    }));
+  });
+});
