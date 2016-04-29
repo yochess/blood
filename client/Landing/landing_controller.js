7069ad23ce3ab67669b889b69142c5be6a899627
John Boucherie <jvboucherie@gmail.com>
2016-04-29 00:08:22 -0700

    Add template landing page and art assets.

--- client/Landing/landing_controller.js
+++ client/Landing/landing_controller.js
@@ -1,8 +1,7 @@
 app.controller('LandingController', ['$routeParams', function($routeParams) {
-
   let LandingCtrl = this;
 
-  LandingCtrl.github = 'https://github.com/britishchicken/blood';
-
+  LandingCtrl.github = 'Github??'
 }]);
+
 
