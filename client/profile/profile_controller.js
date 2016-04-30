b20976f450bb0d1b70facf5a8b9cf6f1c8b663c5
vinithasraja <snellhome1@gmail.com>
2016-04-30 16:13:18 -0700

    Implement getlatlong controller function

--- client/profile/profile_controller.js
+++ client/profile/profile_controller.js
@@ -7,8 +7,9 @@ app.controller('ProfileController', ['$routeParams' , 'Profile', '$rootScope', f
         name: 'Donor',
         email: 'Donor@gmail.com',
         photo: 'http://www.who.int/campaigns/world-blood-donor-day/2013/promotional/tshirt_red_logo1.jpg?ua=1',
-        lat: 'HackReactor, Sanfransisco',
-        long: '',
+        address: '944 Market Street, San Francisco, CA 94102',
+        lat :'37.783697',
+        long: '-122.408966',
         bloodtype: 'O+'
       };
  
@@ -18,6 +19,7 @@ app.controller('ProfileController', ['$routeParams' , 'Profile', '$rootScope', f
     name: '',
     email: '',
     photo:'',
+    address:'',
     lat: '',
     long:'',
     bloodtype: ''
@@ -27,7 +29,29 @@ app.controller('ProfileController', ['$routeParams' , 'Profile', '$rootScope', f
   ProfileCtrl.Edit = function() {
     ProfileCtrl.edit = !ProfileCtrl.edit;
   };
+
  
+  ProfileCtrl.getlatlong = () => {
+    let address = document.getElementById('address').value;
+    console.log(address);
+    // address = address || 'Ferrol, Galicia, Spain';
+    // Initialize the Geocoder
+    let geocoder = new google.maps.Geocoder();
+    if (geocoder) {
+    console.log(geocoder);
+
+        geocoder.geocode({
+            'address': address
+        }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              console.log(results);
+              ProfileCtrl.user.lat = results[0].geometry.location.lat();
+              ProfileCtrl.user.long = results[0].geometry.location.lng();
+            }
+        });
+    }
+};
+
 
   ProfileCtrl.updateUser = () => {
     // console.log('updateUser', ProfileCtrl.user);
@@ -37,7 +61,7 @@ app.controller('ProfileController', ['$routeParams' , 'Profile', '$rootScope', f
       .then((profile) => {
         ProfileCtrl.user = profile;
       })
-      .catch((error) =>{
+      .catch((error) => {
         console.error(error);
       });
   };
@@ -57,6 +81,7 @@ app.controller('ProfileController', ['$routeParams' , 'Profile', '$rootScope', f
     //   console.log(error);
     // });
   };
+
 
   displayUser();
   
