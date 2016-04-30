de71f118239dd8e4e06b951e82ad8c604eb47d04
John Boucherie <jvboucherie@gmail.com>
2016-04-29 17:19:35 -0700

    Create functions to interact with blood level graph.

--- client/stock/stock_controller.js
+++ client/stock/stock_controller.js
@@ -1,7 +1,39 @@
 app.controller('StockController', ['$routeParams', function($routeParams) {
   let StockCtrl = this;
 
-  StockCtrl.message = 'This is the stock controller\'s message.'
+  StockCtrl.levels = {
+    oPos: 4,
+    oNeg: 5,
+    aPos: 15,
+    aNeg: 19,
+    bPos: 8,
+    bNeg: 7,
+    abPos: 10,
+    abNeg: 12
+  }
+
+  StockCtrl.setLevel = 0;
+
+  StockCtrl.currentSelectedType = 'oPos';
+
+  StockCtrl.yAxis = 'Amount';
+  StockCtrl.xAxis = 'Types';
+
+  StockCtrl.max = 0;
+
+  StockCtrl.handleClick = function (keyName) {
+    StockCtrl.currentSelectedType = keyName;
+  }
+
+  for (key in StockCtrl.levels ) {
+    if (StockCtrl.levels[key] > StockCtrl.max) {
+      StockCtrl.max = StockCtrl.levels[key];
+    }
+  }
+
+  StockCtrl.setTypeLevel = function (type) {
+    StockCtrl.levels[StockCtrl.currentSelectedType] = StockCtrl.setLevel;
+  }
 }]);
 
 
