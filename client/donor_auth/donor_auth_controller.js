7b059d4d16aec800d368abe8078d1e2d0d233527
vinithasraja <snellhome1@gmail.com>
2016-05-02 14:26:10 -0700

    Invoke donor login and signup service functions

--- client/donor_auth/donor_auth_controller.js
+++ client/donor_auth/donor_auth_controller.js
@@ -4,21 +4,23 @@ app.controller('DonorAuthController', ['$window','$rootScope', '$http', 'DonorAu
     DonorAuthCtrl.loginObj = {};
 
     DonorAuthCtrl.signup = () => {
-      DonorAuth.signup(DonorAuthCtrl.signupObj);
-      $rootScope.NavCtrl.login();
-      $window.location.assign('#profile');
-    };
+      DonorAuth.signup(DonorAuthCtrl.signupObj)
+      .then((donor) => {
+        console.log('hi>>>', donor);
+        $rootScope.NavCtrl.login();
+
+      });
+    //   $rootScope.NavCtrl.login();
+    //   $window.location.assign('#profile');
+     };
 
     DonorAuthCtrl.login = () => {
-      DonorAuth.login(DonorAuthCtrl.loginObj);
-      // $http({
-      //   method: 'GET',
-      //   url: '/something'
-      // })
-      // .then(() => {
+      console.log('inside controller');
+      DonorAuth.login(DonorAuthCtrl.loginObj)
+      .then((donor) => {
         $rootScope.NavCtrl.login();
-        $window.location.assign('#bloodmap');
-      // });
+
+      });
       
     };
 
