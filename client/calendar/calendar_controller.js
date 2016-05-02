3177b05b835093e71b615b0ee0ef9479ae1420de
Daham Drake Wang <drakewang@gmail.com>
2016-05-02 16:19:06 -0700

    Add AddEvent method.

--- client/calendar/calendar_controller.js
+++ client/calendar/calendar_controller.js
@@ -1,80 +1,102 @@
 (() => {
   app.controller('CalendarController', ['$http', function($http) {
-    let CalendarCtrl = this;
-    let CLIENT_ID = '201136918452-keglqvefmnl0gf5thj0lsp4o720v5f7v.apps.googleusercontent.com';
-    const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+    // let CalendarCtrl = this;
+    // let CLIENT_ID = '201136918452-keglqvefmnl0gf5thj0lsp4o720v5f7v.apps.googleusercontent.com';
+    // const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
-    // maybe not necessary for onload
-    // CalendarCtrl.checkAuth = () => {
-    //   gapi.auth.authorize(
-    //     {
-    //       'client_id': CLIENT_ID,
-    //       'scope': SCOPES.join(' '),
-    //       'immediate': true
-    //     }, CalendarCtrl.handleAuthResult);
+    // // maybe not necessary for onload
+    // // CalendarCtrl.checkAuth = () => {
+    // //   gapi.auth.authorize(
+    // //     {
+    // //       'client_id': CLIENT_ID,
+    // //       'scope': SCOPES.join(' '),
+    // //       'immediate': true
+    // //     }, CalendarCtrl.handleAuthResult);
+    // // };
+
+    // CalendarCtrl.handleAuthClick = (event) => {
+    //   gapi.auth.authorize({
+    //     client_id: CLIENT_ID, 
+    //     scope: SCOPES, 
+    //     immediate: false
+    //   }, CalendarCtrl.handleAuthResult);
     // };
 
-    CalendarCtrl.handleAuthClick = (event) => {
-      gapi.auth.authorize({
-        client_id: CLIENT_ID, 
-        scope: SCOPES, 
-        immediate: false
-      }, CalendarCtrl.handleAuthResult);
-    };
+    // CalendarCtrl.handleAuthResult = (authResult) => {
+    //   let authorizeDiv = document.getElementById('authorize-div');
+    //   if (authResult && !authResult.error) {
+    //     CalendarCtrl.loadCalendarApi();
+    //   } else {
+    //     // not sure yet
+    //     console.log('something not working!');
+    //   }
+    // };
 
-    CalendarCtrl.handleAuthResult = (authResult) => {
-      let authorizeDiv = document.getElementById('authorize-div');
-      if (authResult && !authResult.error) {
-        CalendarCtrl.loadCalendarApi();
-      } else {
-        // not sure yet
-        console.log('something not working!');
-      }
-    };
+    // CalendarCtrl.loadCalendarApi = () => {
+    //   gapi.client.load('calendar', 'v3', CalendarCtrl.listUpcomingEvents);
+    // };
 
-    CalendarCtrl.loadCalendarApi = () => {
-      gapi.client.load('calendar', 'v3', CalendarCtrl.listUpcomingEvents);
-    };
+    // CalendarCtrl.listUpcomingEvents = () => {
+    //   let request = gapi.client.calendar.events.list({
+    //     'calendarId': 'primary',
+    //     'timeMin': (new Date()).toISOString(),
+    //     'showDeleted': false,
+    //     'singleEvents': true,
+    //     'maxResults': 15,
+    //     'orderBy': 'startTime'
+    //   });
 
-    CalendarCtrl.listUpcomingEvents = () => {
-      let request = gapi.client.calendar.events.list({
-        'calendarId': 'primary',
-        'timeMin': (new Date()).toISOString(),
-        'showDeleted': false,
-        'singleEvents': true,
-        'maxResults': 15,
-        'orderBy': 'startTime'
+    //   request.execute(function(resp) {
+    //     var events = resp.items;
+    //     appendPre('The up comming events in your calendar are:'+ '\n'+'\n');
+
+
+
+    //     if (events.length > 0) {
+    //       for (i = 0; i < events.length; i++) {
+
+    //         var event = events[i];
+    //         var when = event.start.dateTime;
+
+    //         if (!when) {
+    //           when = event.start.date;
+    //         }
+    //         appendPre(i+1+' '+ event.summary + ' ('+' '+ when +' '+ ')' + '\n');
+    //       }
+    //     } else {
+    //       appendPre('No upcoming events found.');
+    //     }
+
+    //   });
+
+    //   function appendPre(message) {
+    //     var pre = document.getElementById('output');
+    //     var textContent = document.createTextNode(message + '\n');
+    //     pre.appendChild(textContent);
+    //   }
+
+    // }
+    let CalendarCtrl = this;
+    CalendarCtrl.showAllEvents = () => {
+      $http({
+        method: 'GET',
+        url: '/api/calendar/'
+      })
+      .then(res => {
+        console.log(res);
+        CalendarCtrl.events = res.data;
+        console.log(CalendarCtrl.events);
       });
+    };
 
-      request.execute(function(resp) {
-        var events = resp.items;
-        appendPre('The up comming events in your calendar are:'+ '\n'+'\n');
-
-
-
-        if (events.length > 0) {
-          for (i = 0; i < events.length; i++) {
-
-            var event = events[i];
-            var when = event.start.dateTime;
-
-            if (!when) {
-              when = event.start.date;
-            }
-            appendPre(i+1+' '+ event.summary + ' ('+' '+ when +' '+ ')' + '\n');
-          }
-        } else {
-          appendPre('No upcoming events found.');
-        }
-
+    CalendarCtrl.createEvent = () => {
+      $http({
+        method: 'POST',
+        url: '/api/calendar/'
       });
+    };
 
-      function appendPre(message) {
-        var pre = document.getElementById('output');
-        var textContent = document.createTextNode(message + '\n');
-        pre.appendChild(textContent);
-      }
 
-    }
+    CalendarCtrl.showAllEvents();
   }]);
 })();
