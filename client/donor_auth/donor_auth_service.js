4d7d2b198f4b1e600940addc54f5110ab6e5422d
vinithasraja <snellhome1@gmail.com>
2016-05-02 14:29:12 -0700

    Fix the data and routes for http post service functions

--- client/donor_auth/donor_auth_service.js
+++ client/donor_auth/donor_auth_service.js
@@ -2,33 +2,36 @@
   app.factory('DonorAuth', function($http, $window) {
 
     let signup = (user) => {
-      // return $http({
-      //   method: 'POST',
-      //   url: '/auth/donor/signup',
-      //   data: user
-      // })
-      // .then(res => {
-      //   if (res.status === 200) {
-      //     $window.location.assign('#donor/edit');
-      //   } else {
-      //     $window.location.assign('#donor/signup');
-      //   }
-      // });
+      return $http({
+        method: 'POST',
+        url: '/auth/donor/signup',
+        data: user
+      })
+      .then(res => {
+        if (res.status === 200) {
+          $window.location.assign('#profile');
+        } else {
+          $window.location.assign('#donor/signup');
+        }
+      });
     };
 
     let login = (user) => {
-      // return $http({
-      //   method: 'POST',
-      //   url: '/auth/donor/login',
-      //   data: user
-      // })
-      // .then(res => {
-      //   if (res.status === 200) {
-      //     $window.location.assign('#donor/edit');
-      //   } else {
-      //     $window.location.assign('#donor/signup');
-      //   }
-      // });
+      return $http({
+        method: 'POST',
+        url: '/auth/donor/login',
+        data: user
+      })
+      .then(res => {
+        if (res.status === 200) {
+          $window.location.assign('#profile');
+        } else {
+          $window.location.assign('#donor/signup');
+        }
+      })
+      .catch(error => {
+        console.log("user does not exists");
+      });
     };
 
     return {signup, login};
