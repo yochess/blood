e00af676406041ed53341e8608a5317890fbdff9
vinithasraja <snellhome1@gmail.com>
2016-05-03 16:09:23 -0700

    Work to get the hospital data with get method

--- client/bloodMap/bloodMap_service.js
+++ client/bloodMap/bloodMap_service.js
@@ -1,12 +1,15 @@
 app.factory('BloodMap', function($http) {
 
   let getMap = (geoobj) => {
+    console.log('service geoobj', geoobj);
     return $http({
       method: 'GET',
       url: '/api/hospital/geo',
       params: geoobj// {H: xd, j: qd}
     })
     .then((res) => {  
+    console.log(res.data);
+    
       return res.data;
     });
   };
