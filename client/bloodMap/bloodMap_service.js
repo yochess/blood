6da1b4e4fb45b5374fb4e6b32ed1a8c0b19de4e5
vinithasraja <snellhome1@gmail.com>
2016-05-03 17:28:02 -0700

    Functional getMap service function to get the hospital list

--- client/bloodMap/bloodMap_service.js
+++ client/bloodMap/bloodMap_service.js
@@ -1,15 +1,12 @@
 app.factory('BloodMap', function($http) {
 
   let getMap = (geoobj) => {
-    console.log('service geoobj', geoobj);
     return $http({
       method: 'GET',
       url: '/api/hospital/geo',
-      params: geoobj// {H: xd, j: qd}
+      params: geoobj
     })
     .then((res) => {  
-    console.log(res.data);
-    
       return res.data;
     });
   };
