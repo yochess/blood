069d1beb9d3a8c416002706b1b2fd9cef6f9dd6d
Travis Baratcart <baratcart.travis@gmail.com>
2016-05-09 11:32:27 -0700

    Catch error and log more.

--- server/controllers/event.js
+++ server/controllers/event.js
@@ -10,6 +10,7 @@ let Sequelize = require('sequelize');
 const FIFTYSIXDAYS = 5E9; // basically
 
 let postEvent = (req, res) => {
+  console.log('in controller');
   Promise.all([
     Event.create({time: req.body.time}),
     Donor.findOne({where: {id: req.user.id}})
@@ -19,7 +20,8 @@ let postEvent = (req, res) => {
       let donor = results[1];
       event.addDonor(donor)
       .then(() => res.send(event));
-    });
+    })
+    .catch(err => console.log(err));
   };
 
 let getEventsByLocation = (req, res) => {
