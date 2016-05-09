0a630f02e1f5e2d5d25df887cbbb7cf0a446e107
Travis Baratcart <baratcart.travis@gmail.com>
2016-05-09 15:36:33 -0700

    Add links.

--- client/bloodMap/event_controller.js
+++ client/bloodMap/event_controller.js
@@ -3,10 +3,7 @@ app.controller('MapEventController', ['$scope', 'MapEvent', function($scope, Map
   MapEventCtrl.events = [];
 
   let getEvents = () => {
-    MapEvent.get($scope.bounds.H.H, $scope.bounds.j.j, $scope.bounds.H.j, $scope.bounds.j.H).then(events => {
-      MapEventCtrl.events = events;
-      console.log(events);
-    });
+    MapEvent.get($scope.bounds.H.H, $scope.bounds.j.j, $scope.bounds.H.j, $scope.bounds.j.H).then(events => MapEventCtrl.events = events);
   };
 
   setTimeout(getEvents, 1000);
