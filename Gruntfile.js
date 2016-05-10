49ce1eef58d82dc86f2907199bea5d1c2a7dc637
Travis Baratcart <baratcart.travis@gmail.com>
2016-05-10 15:15:52 -0700

    Add gruntfile and use CDNs for libs.

---
+++ Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+
+// Project configuration.
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  uglify: {
+    options: {
+      banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+    },
+    build: {
+      src: 'client/dist/build_es5.js',
+      dest: 'client/dist/build_min.js'
+    },
+  },
+  concat: {
+    options: {
+      separator: ';',
+    },
+    dist: {
+      src: ['client/app.js', 'client/*/*_controller.js', 'client/*/*_service.js'],
+      dest: 'client/dist/build.js',
+    },
+  },
+  babel: {
+    options: {
+      sourceMap: true,
+      presets: ['es2015']
+    },
+    dist: {
+      files: {
+        'client/dist/build_es5.js': 'client/dist/build.js'
+      }
+    }
+  }
+});
+
+// Load the plugin that provides the "uglify" task.
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-babel');
+
+// Default task(s).
+grunt.registerTask('default', ['concat', 'babel', 'uglify']);
+
+};
