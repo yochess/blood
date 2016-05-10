f3e49d390e3b97e54113cbc61a19b07c6717569f
vinithasraja <snellhome1@gmail.com>
2016-05-10 15:43:53 -0700

    Implement basic D3 BarHorizontalChart

--- client/bloodMap/bloodMap_controller.js
+++ client/bloodMap/bloodMap_controller.js
@@ -70,6 +70,7 @@ app.controller('BloodMapController', ['$window','$routeParams' , '$rootScope', '
     $scope.data = [
       {
         key: "Blood Levels",
+        "color": "#1f77b4",
         values: [
                 {
                   "label" : "O+" ,
@@ -110,6 +111,57 @@ app.controller('BloodMapController', ['$window','$routeParams' , '$rootScope', '
     ////////////////////////////////////////
     //  FAREWELL, FROM THE FORBIDDEN ZONE.
     ////////////////////////////////////////
+ var donordata = [
+           {
+            key: 'Series1',
+            values: [
+                {
+                    "label" : "Donor1" ,
+                    "value" : 20
+                } ,
+                {
+                    "label" : "Donor2" ,
+                    "value" : 15
+                } ,
+                {
+                    "label" : "Donor3" ,
+                    "value" : 10
+                } ,
+                {
+                    "label" : "Donor4" ,
+                    "value" : 5
+                } ,
+                {
+                    "label" : "Donor5" ,
+                    "value" : 2
+                }
+            ]
+          }
+
+     ];
+    let chart;
+    nv.addGraph(function() {
+        chart = nv.models.multiBarHorizontalChart()
+            .x(function(d) { return d.label })
+            .y(function(d) { return d.value })
+            .yErr(function(d) { return [-Math.abs(d.value * Math.random() * 0.3), Math.abs(d.value * Math.random() * 0.3)] })
+            .barColor(d3.scale.category20().range())
+            .duration(250)
+            .margin({left: 100})
+            .stacked(true);
+        chart.yAxis.tickFormat(d3.format(',.2f'));
+        chart.yAxis.axisLabel('Y Axis');
+        chart.xAxis.axisLabel('X Axis').axisLabelDistance(20);
+        d3.select('#chart1 svg')
+            .datum(donordata)
+            .call(chart);
+        nv.utils.windowResize(chart.update);
+        chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+        chart.state.dispatch.on('change', function(state){
+            nv.log('state', JSON.stringify(state));
+        });
+        return chart;
+    });
 
 
     GeoMarker.setMap(BloodMapCtrl.map);
