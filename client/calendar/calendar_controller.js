0dafdca3a33124f7ad6c4c780f8fcc16c78d35f7
Daham Drake Wang <drakewang@gmail.com>
2016-05-11 10:37:30 -0700

    Modularize calendar done.

--- client/calendar/calendar_controller.js
+++ client/calendar/calendar_controller.js
@@ -3,61 +3,174 @@
     let CalendarCtrl = this;
     let $calendar = $('#calendar');
 
-    CalendarCtrl.possibleEvents = [];
-    CalendarCtrl.googleEvents = [];
     CalendarCtrl.time = {};
-    CalendarCtrl.isLoggedin = false;
-
 
     $calendar.fullCalendar({
       timezone: 'local',
       displayEventEnd: true,
       events: (start, end, timezone, callback) => {
         Calendar.getCalendarEvents().then(res => {
-          callback(res.data);
+          CalendarCtrl.isLoggedin = true;
+          CalendarCtrl.googleEvents = res.data;
+          CalendarCtrl.fillCalendar(callback);
         }).catch(err => {
-          console.log('you are probably not logged in: ', err);
+          CalendarCtrl.isLoggedin = false;
+          CalendarCtrl.googleEvents = [];
+          CalendarCtrl.fillCalendar(callback);
         });
       },
       eventClick: (calEvent, jsEvent, view) => {
         if (!CalendarCtrl.isLoggedin) {
+          return console.log('you are not logged in!');
         }
-
         // this should be less hack-ish
         if (calEvent.title === 'Slot Available') {
           let $box1 = $('.modal.box1');
-          // console.log(calEvent.title);
-          // modal.find('.modal-title').html('Schedule an Appointment');
           CalendarCtrl.setView(calEvent);
           $scope.$apply();
           $box1.modal();
-          // modal.find('.modal-title').html('Schedule an Appointment').modal();
-
         }
       }
     });
 
     CalendarCtrl.googleSignin = () => {
-      $http.get('/auth/url').then(res => {
-        let url = res.data;
-        let newWindow = $window.open(url, 'AuthPage', 'width=500px,height=700px');
-
-        // grabs message with code from oauthcallback.html and stores it in `e`
+      Calendar.getUrl().then(res => {
+        let newWindow = $window.open(res.data, 'AuthPage', 'width=500px,height=700px');
+        // e.data is a string with the code
         $window.onmessage = (e) => {
-          let urlWithCode = e.data;
-          let index = urlWithCode.lastIndexOf('code=');
-          let code = urlWithCode.substring(index + 5).replace('#', '');
+          let index = e.data.lastIndexOf('code=');
+          let code = e.data.substring(index + 5).replace('#', '');
           newWindow.close();
-
-          $http.get('/auth/googleToken?code=' + code).then(res => {
+          Calendar.getToken(code).then(res => {
             console.log('You are authenticated!');
-            // CalendarCtrl.getGoogleData();
-            CalendarCtrl.isLoggedin = true;
             $calendar.fullCalendar('refetchEvents');
-            // $window.localStorage.setItem('signedin', true);
           });
         };
       });
+    };
+
+    CalendarCtrl.fillCalendar = (callback) => {
+      // this is hacky currently and can be optimized
+      $calendar.fullCalendar('removeEvents');
+      $calendar.fullCalendar('addEventSource', CalendarCtrl.googleEvents);
+      CalendarCtrl.getAppointmentData(CalendarCtrl.checkOverlap, callback);
+    };
+
+    CalendarCtrl.reload = () => {
+      $calendar.fullCalendar('refetchEvents');
+    };
+
+    CalendarCtrl.getAppointmentData = (checkOverlap, callback) => {
+      Calendar.getHospitalProfile($routeParams.hospitalid).then(res => {
+        if (!res.data) {
+          callback(null);
+          return console.log('no hospital id in params!');
+        }
+        let currentDate = new Date();
+        let dayIndex, startHour, endHour;
+        CalendarCtrl.appointments = _.flatten(_.range(31).map(counter => {
+          currentDate = new Date(currentDate.getTime() + (1000 * 60 * 60 * 24));
+          dayIndex = (currentDate.getDay() + 6) % 7;
+          return {
+            currentDate: currentDate,
+            openhour: res.data.schedules[dayIndex].openhours,
+            endhour: res.data.schedules[dayIndex].closehours,
+          };
+        }).filter(data => {
+          return !!data.openhour;
+        }).map(data => {
+          return _.range(data.openhour, data.endhour).map(hour => {
+            return {
+              title: 'Slot Available',
+              start: data.currentDate.setHours(hour, 0, 0, 0),
+              backgroundColor: '#378006'
+            };
+          });
+        }));
+        checkOverlap(CalendarCtrl.appointments, CalendarCtrl.googleEvents);
+        callback(CalendarCtrl.appointments);
+
+      })
+    };
+
+    CalendarCtrl.checkOverlap = (appointments, googleEvents) => {
+      for (let googleEvent of googleEvents) {
+        let isOverlap = false;
+        let startIndex = 0;
+        let endIndex = appointments.length -1;
+        let midIndex, appointment, start, end;
+
+        while (!isOverlap && (startIndex <= endIndex)) {
+          midIndex = Math.floor((startIndex + endIndex) / 2);
+          startTime = new Date(googleEvent.start).getTime();
+          endTime = new Date(googleEvent.end).getTime();
+          appointmentTime = appointments[midIndex].start;
+
+          if((appointmentTime >= startTime) && (appointmentTime <= endTime)) {
+            isOverlap = true;
+          } else if (appointmentTime < startTime) {
+            startIndex = midIndex + 1;
+          } else if (appointmentTime > endTime) {
+            endIndex = midIndex - 1;
+          } else {
+            console.log('wtf, this is not suppose to happen!');
+            break;
+          }
+        }
+
+        if (isOverlap) {
+          CalendarCtrl.removeEventData(appointments, midIndex);
+        }
+      }
+    };
+
+    CalendarCtrl.createEvent = (startDate, endDate) => {
+      startDate = startDate || CalendarCtrl.dateTime;
+      endDate = endDate || CalendarCtrl.dateTime || startDate;
+
+      Calendar.postCalendarEvent(startDate, endDate).then(res => {
+        if (res.status === 201) {
+          $calendar.fullCalendar('refetchEvents');
+        }
+      });
+    };
+
+    CalendarCtrl.removeEventData = (events, index) => {
+      console.log('removed!');
+      CalendarCtrl.appointments.splice(index, 1);
+    };
+
+    CalendarCtrl.setView = (calEvent) => {
+      let month = calEvent.start.month();
+      let day = calEvent.start.day();
+      let hour = calEvent.start.hour();
+      let minute = calEvent.start.minutes();
+      minute = minute < 10 ? minute + '0' : minute;
+
+      CalendarCtrl.time.start = calEvent.start;
+      CalendarCtrl.time.end = calEvent.end;
+
+      CalendarCtrl.time.print = `${month}/${day} @ ${hour}:${minute}`;
+    };
+
+    CalendarCtrl.firstModal = () => {
+      let $box2 = $('.modal.box2');
+      let $inputs = $('.modal').find('input');
+
+      $inputs.each((index, input) => {
+        input.checked = false;
+      });
+
+      $box2.modal();
+    };
+
+    CalendarCtrl.secondModal = () => {
+      let $input1 = $('.checkbox.input1');
+      let $input2 = $('.checkbox.input2');
+
+      // console.log($input1.find('input'));
+      // console.log($input2.find('input'));
+      CalendarCtrl.createEvent(CalendarCtrl.time.start, CalendarCtrl.time.end);
     };
 
 
--- client/calendar/calendar_service.js
+++ client/calendar/calendar_service.js
@@ -1,16 +1,62 @@
 (() => {
   app.service('Calendar', ['$http', function($http) {
     let getCalendarEvents = () => {
-      return $http.get('/api/calendar').then(res => {
-        return res;
+      return $http({
+        url: '/api/calendar',
+        method: 'GET'
       });
-
     };
 
+    let postCalendarEvent = (startDate, endDate) => {
+      return $http({
+        url: '/api/calendar',
+        method: 'POST',
+        data: {
+          summary: 'Blood',
+          location: '800 Howard St., San Francisco, CA 94103',
+          description: 'A chance to hear more about Google\'s developer products.',
+          start: {
+            dateTime: startDate,
+            timeZone: 'America/Los_Angeles',
+          },
+          end: {
+            dateTime: endDate,
+            timeZone: 'America/Los_Angeles'
+          }
+        }
+      });
+    };
 
+    let getHospitalProfile = (hospitalId) => {
+      return $http({
+        url: `/api/hospital/profile/${hospitalId}`,
+        method: 'GET'
+      });
+    };
+
+    // this may need to be separated to an auth service
+    let getUrl = () => {
+      return $http({
+        url: '/auth/url',
+        method: 'GET'
+      });
+    }
+
+    // this may need to be separated to an auth service
+    let getToken = (code) => {
+      return $http({
+        url: '/auth/googleToken',
+        method: 'GET',
+        params: {code: code}
+      });
+    };
 
     return {
-      getCalendarEvents: getCalendarEvents
+      getCalendarEvents: getCalendarEvents,
+      getHospitalProfile: getHospitalProfile,
+      getToken: getToken,
+      getUrl: getUrl,
+      postCalendarEvent: postCalendarEvent
     };
 
   }]);
