8953437c484f009b104e322f3e0af04fb562015b
Travis Baratcart <baratcart.travis@gmail.com>
2016-05-11 15:30:58 -0700

    Probably fix deploy.

--- server/server.js
+++ server/server.js
@@ -52,11 +52,10 @@ app.get('/', function(req, res) {
 });
 
 if (config.production) {
-
   let options = {
-    key: fs.readFileSync('../tls/key.pem'),
-    cert: fs.readFileSync('../tls/cert.pem'),
-    ca: fs.readFileSync('../tls/fullchain.pem')
+    key: fs.readFileSync(__dirname + '../tls/key.pem'),
+    cert: fs.readFileSync(__dirname + '../tls/cert.pem'),
+    ca: fs.readFileSync(__dirname + '../tls/fullchain.pem')
   };
 
   let server = https.createServer(options, app);
