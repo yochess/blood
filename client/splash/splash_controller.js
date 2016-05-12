137558af6f92980c30487304cd9a98d9ea46f8c8
John Boucherie <jvboucherie@gmail.com>
2016-05-12 14:44:46 -0700

    Add data visualization to splash page.

--- client/splash/splash_controller.js
+++ client/splash/splash_controller.js
@@ -1,7 +1,76 @@
-app.controller('SplashController', ['$routeParams', function($routeParams) {
+app.controller('SplashController', ['$routeParams', '$scope', function($routeParams, $scope) {
   let SplashCtrl = this;
 
-  SplashCtrl.github = 'Github??'
-}]);
+  loadLiquidFillGauge("fillgauge2", 28);
 
+      ////////////////////////////////////////
+    //       BEHOLD, THE FORBIDDEN ZONE.
+    ////////////////////////////////////////
+
+
+    $scope.options = {
+      chart: {
+        type: 'discreteBarChart',
+        height: 200,
+        x: function(d){return d.label;},
+        y: function(d){return d.value;},
+        showValues: true,
+        color: function(){return '#700000';},
+        valueFormat: function(d){ return d3.format(',f')(d); },
+        dispatch: {
+
+        },
+        discretebar: {
+          dispatch: {
+            elementClick: function(e) {$scope.$apply();}
+            }
+          },
+        callback: function(e){console.log('! callback !')}
+      }
+    };
+
+    $scope.data = [
+      {
+        key: "Blood Levels",
+        values: [
+                {
+                  "label" : "O+" ,
+                  "value" : 6
+                },
+                {
+                  "label" : "O-" ,
+                  "value" : 5
+                },
+                {
+                  "label" : "A+" ,
+                  "value" : 18
+                },
+                {
+                  "label" : "A-" ,
+                  "value" : 8
+                },
+                {
+                  "label" : "B+" ,
+                  "value" : 17
+                },
+                {
+                  "label" : "B-" ,
+                  "value" : 7
+                },
+                {
+                  "label" : "AB+" ,
+                  "value" : 16
+                },
+                {
+                  "label" : "AB-" ,
+                  "value" : 6
+                }
+        ]
+      }
+    ];
+
+    ////////////////////////////////////////
+    //  FAREWELL, FROM THE FORBIDDEN ZONE.
+    ////////////////////////////////////////
+}]);
 
