f2fb418f2d36f333d3b9b102b705ec8bcb46efa7
vinithasraja <snellhome1@gmail.com>
2016-05-13 15:06:12 -0700

    Buddy middleware function to get buddy request

---
+++ server/controllers/buddy.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let db = require('../controllers/controller.js');
+let Donor = db.Donor;
+let Hospital = db.Hospital;
+let Buddy = db.Buddy;
+
+let requestBuddy = (req, res) => {
+  let donorId = req.user.type === 'donor' ? req.user.id : null;
+  //let hospitalId = req.user.type === 'hospital' ? req.user.id : null;
+  Donor.findOne({where: {id: req.user.id}})
+  .then(donor => {
+    Buddy.create({
+      time: req.body.time,
+      found: false,
+      donorId: donorId,
+      hospitalId: req.body.hospitalid})
+    .then(buddy => res.send(buddy));
+  });
+};
+
+let getBuddy = (req, res) => {
+  Buddy.findOne({where: {donorId: req.user.id}})
+  .then(buddy => res.send(buddy));
+
+};
+module.exports.getBuddy = getBuddy;
+module.exports.requestBuddy = requestBuddy;
