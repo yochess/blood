a070cfce4e38818188c5137a1cdbc1f0be24c187
vinithasraja <snellhome1@gmail.com>
2016-05-13 17:18:19 -0700

    Add buddy id tothe path

--- client/app.js
+++ client/app.js
@@ -100,7 +100,7 @@ let app = angular.module('blood', ['ngRoute', 'nvd3', 'rzModule'])
       }]
     }
   })
-  .when('/bloodbuddy', {
+  .when('/bloodbuddy/:buddyId', {
     templateUrl: 'calendar/blood_buddy.html',
     controller: 'BuddyController',
     controllerAs: 'BuddyCtrl'
