cc27f7f099b92046565557e9bb70590c9138f91f
vinithasraja <snellhome1@gmail.com>
2016-05-14 11:42:00 -0700

    Update the blood buddy table and implement http put

--- client/calendar/blood_buddy_controller.js
+++ client/calendar/blood_buddy_controller.js
@@ -1,17 +1,26 @@
 app.controller('BuddyController', ['$window' , '$scope','$routeParams', 'Buddy', function($window,  $scope,$routeParams, Buddy) {
   let BuddyCtrl = this;
+  BuddyCtrl.found = false;
   console.log("BuddyCtrl");
 
   BuddyCtrl.buddy = {
     name: 'Blood Buddy'
   };
 
+  BuddyCtrl.submit = () => {
+    Buddy.update($routeParams.buddyId, BuddyCtrl.email)
+    .then(buddy => {
+      BuddyCtrl.found = true;
+      //get buddy and schedule an appointment for both donor and buddy
+    });
+  }
   let getBuddy = () => {
     Buddy.get($routeParams.buddyId)
     .then(buddy => {
       BuddyCtrl.buddy = buddy;
     console.log(buddy);
   });
+
   };
 
 
