04d5ad171b28b970d224c2ae88a5aab770fb092f
vinithasraja <snellhome1@gmail.com>
2016-05-13 17:26:06 -0700

    Add routes to invoke buddyrouter middleware function

--- server/routes/buddy.js
+++ server/routes/buddy.js
@@ -3,7 +3,10 @@ let buddyRouter = require('express').Router();
 let buddyControllers = require('../controllers/buddy.js');
 
 buddyRouter.route('/')
-.get(buddyControllers.getBuddy)
+//.get(buddyControllers.getBuddy)
 .post(buddyControllers.requestBuddy);
+
+buddyRouter.route('/:id')
+.get(buddyControllers.getBuddy);
 
 module.exports = buddyRouter;
