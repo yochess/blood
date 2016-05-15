247c9207d0942352b71ef0e90ef296d70f8de200
vinithasraja <snellhome1@gmail.com>
2016-05-14 17:19:27 -0700

    Update and create appointments for buddy and donor

--- client/calendar/blood_buddy_controller.js
+++ client/calendar/blood_buddy_controller.js
@@ -1,6 +1,6 @@
-app.controller('BuddyController', ['$window' , '$scope','$routeParams', 'Buddy', function($window,  $scope,$routeParams, Buddy) {
+app.controller('BuddyController', ['$window' , '$scope','$routeParams', 'Buddy','Calendar', function($window,  $scope,$routeParams, Buddy, Calendar) {
   let BuddyCtrl = this;
-  BuddyCtrl.found = false;
+  //BuddyCtrl.found = false;
   console.log("BuddyCtrl");
 
   BuddyCtrl.buddy = {
@@ -10,7 +10,11 @@ app.controller('BuddyController', ['$window' , '$scope','$routeParams', 'Buddy',
   BuddyCtrl.submit = () => {
     Buddy.update($routeParams.buddyId, BuddyCtrl.email)
     .then(buddy => {
-      BuddyCtrl.found = true;
+      if(buddy.buddyId !== null){
+        BuddyCtrl.found= true;
+      } else {
+        BuddyCtrl.found= false;
+      }
       //get buddy and schedule an appointment for both donor and buddy
     });
   }
@@ -18,7 +22,11 @@ app.controller('BuddyController', ['$window' , '$scope','$routeParams', 'Buddy',
     Buddy.get($routeParams.buddyId)
     .then(buddy => {
       BuddyCtrl.buddy = buddy;
-    console.log(buddy);
+      if(buddy.buddyId !== null){
+        BuddyCtrl.found= true;
+      } else {
+        BuddyCtrl.found= false;
+      }
   });
 
   };
