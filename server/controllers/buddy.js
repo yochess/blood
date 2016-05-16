f9d9319108c0b672e4279627e6b133baf6d0fdb3
vinithasraja <snellhome1@gmail.com>
2016-05-16 12:11:41 -0700

    Fix budid foreign key

--- server/controllers/buddy.js
+++ server/controllers/buddy.js
@@ -8,39 +8,49 @@ let Appointment = db.Appointment;
 
 let requestBuddy = (req, res) => {
   let donorId = req.user.type === 'donor' ? req.user.id : null;
-  //let hospitalId = req.user.type === 'hospital' ? req.user.id : null;
-  Donor.findOne({where: {id: req.user.id}})
-  .then(donor => {
-    Buddy.create({
-      time: req.body.time,
-      buddyId: null,
-      donorId: donorId,
-      hospitalId: req.body.hospitalid})
-    .then(buddy => res.send(buddy));
-  });
+  Buddy.create({
+    time: req.body.time,
+    donorId: donorId,
+    hospitalId: req.body.hospitalid})
+  .then(buddy => res.send(buddy));
 };
 
 
 let getBuddy = (req, res) => {
-  Buddy.findOne({where: {id: req.params.id},
-    include: [Donor, Hospital]})
+  Buddy.findOne({where:
+    {id: req.params.id},
+    include: [{
+      model: Donor,
+      as: 'donor'
+    }, {
+      model: Donor,
+      as: 'bud'
+    },
+    Hospital]})
   .then(buddy => res.send(buddy));
 
 };
 
 let updateBuddy = (req, res) => {
   Buddy.findOne({where: {id: req.params.id},
-    include: [Donor, Hospital]})
+    include: [{
+      model: Donor,
+      as: 'donor'
+    }, {
+      model: Donor,
+      as: 'bud'
+    },
+    Hospital]})
   .then(buddy => {
     Promise.all([
-    Appointment.create({time: buddy.time, donorId: buddy.donorId, hospitalId: buddy.hospitalId, type: 3}),
-    Appointment.create({time: buddy.time, donorId: req.user.id, hospitalId: buddy.hospitalId, type: 3}),
-    Buddy.update({
-      buddyId: req.user.id
-    } ,{where: {id: req.params.id}})
-    ])
+      Appointment.create({time: buddy.time, donorId: buddy.donorId, hospitalId: buddy.hospitalId, type: 3}),
+      Appointment.create({time: buddy.time, donorId: req.user.id, hospitalId: buddy.hospitalId, type: 3}),
+      Buddy.update({
+        budId: req.user.id
+      } ,{where: {id: req.params.id}})
+      ])
     .then((buddy) =>res.send(buddy));
-    });
+  });
 };
 
 module.exports.getBuddy = getBuddy;
